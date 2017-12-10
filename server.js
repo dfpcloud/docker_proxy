75d7b2ab4a78da35433eae56d6bb45666c55eb7f
akash <akash.sinha29@gmail.com>
2017-12-10 17:14:18 +0530

    updated

--- server.js
+++ server.js
@@ -1,29 +1,32 @@
-var express  = require('express');
-var app      = express();
+var express = require('express');
+var app = express();
 var httpProxy = require('http-proxy');
 var apiProxy = httpProxy.createProxyServer();
 var serverOne = 'http://localhost:4243';
 const env = process.env.dfp_token || 'blank';
-const reg_token = process.env.token;
-//const url = process.env.ecr_url;
+const reg_token = process.env.ecr_token || null;
 
-app.all("/*", function(req, res, next){
+app.all("/*", function (req, res, next) {
     var token = req.get('X-DFP-Token');
-    if(token === env){
+    if (token === env) {
         next();
-    }else {
+    } else {
         return res.status(401).send({
             "message": "Invalid Token",
             "statusCode": "401",
             "data": null
-        }); 
+        });
     }
 })
- 
-app.all("/v1.32/*", function(req, res) {
-	var auth = new Buffer(reg_token).toString('base64');
-	req.headers['X-Registry-Auth'] = auth;
-    apiProxy.web(req, res, {target: serverOne});
+
+app.all("/v1.32/*", function (req, res) {
+    if (reg_token !== null && typeof(reg_token) == "string") {
+        var auth = new Buffer(reg_token).toString('base64');
+        if (!req.headers['X-Registry-Auth']) {
+            req.headers['X-Registry-Auth'] = auth;
+        }
+    }
+    apiProxy.web(req, res, { target: serverOne });
 });
 
 app.listen(8080);
