2d1a2b56877d97b8af184ec44684ef999a684304
akash <akash.sinha29@gmail.com>
2017-12-11 12:07:01 +0530

    added exec

--- server.js
+++ server.js
@@ -3,8 +3,24 @@ var app = express();
 var httpProxy = require('http-proxy');
 var apiProxy = httpProxy.createProxyServer();
 var serverOne = 'http://localhost:4243';
-const env = process.env.dfp_token || 'blank';
-const reg_token = process.env.ecr_token || null;
+var env = process.env.dfp_token || 'blank';
+var child = require('child_process');
+var auth;
+
+child.exec("sudo -u root -H sh -c 'aws ecr get-login --no-include-email'", (err, stdout, stderr) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    var subStr = stdout.split(" ");
+    var data = {
+        "username": subStr[3],
+        "password": subStr[5],
+        "serveraddress": subStr[6].replace(/(\r\n|\n|\r)/gm, "")
+    }
+    auth = new Buffer(JSON.stringify(data)).toString('base64');
+});
+
 
 app.all("/*", function (req, res, next) {
     var token = req.get('X-DFP-Token');
@@ -20,11 +36,8 @@ app.all("/*", function (req, res, next) {
 })
 
 app.all("/v1.32/*", function (req, res) {
-    if (reg_token !== null && typeof(reg_token) == "string") {
-        var auth = new Buffer(reg_token).toString('base64');
-        if (!req.headers['X-Registry-Auth']) {
-            req.headers['X-Registry-Auth'] = auth;
-        }
+    if (!req.headers['X-Registry-Auth']) {
+        req.headers['X-Registry-Auth'] = auth;
     }
     apiProxy.web(req, res, { target: serverOne });
 });
